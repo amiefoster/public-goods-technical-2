4cfcb017c578d544b5e98a9a81fe279ce8240353
Amie Foster <amie.n.foster@gmail.com>
2022-01-03 10:30:31 -0800

    finished

--- index.js
+++ index.js
@@ -33,3 +33,18 @@ let closeWindow = () => {
 getFacts.addEventListener('click', fetchData)
 closeButton.addEventListener('click', closeWindow)
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+//Things I would work on given more time
+
+//I didnt get to any of the CSS styling but given more time I would have made the whole "pop up" into a modal component 
+//I'd use bootstraps grid to organize the modal and place the picture on the left and the rest on the right
+//I would have added some css styling to center the modal itsself and style the inner elements as close to the photo reference as possible!
+
+//Next, I would work on making the modal pop-up
+//I would try to use a combination of CSS and JS. 
+//I would start by using CSS to hide the pop up, and on page load I would add the timer to wait 3 seconds and then unhide the pop up. 
+//I would use CSS animation to make the pop up come up from the bottom of the screen. 
+
+//Thank you for the oppourtunity to interview with Public Goods and I look forward to hearing from you! :) 
