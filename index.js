e962365eb3ea10d0f65bc58cc8724aedc508cd22
Amie Foster <amie.n.foster@gmail.com>
2022-01-03 10:14:20 -0800

    added delete button

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const BASE_URL = "https://cat-fact.herokuapp.com/facts";
 const closeButton = document.querySelector('#close-button');
 const getFacts = document.querySelector('#get-facts');
-const factsContainer = document.querySelector('#facts-container');
-const factsUl = document.querySelector('#fact-ul')
+const factsUl = document.querySelector('#fact-ul');
+const popUp = document.querySelector('#pop-up');
 
 let init = () => {
     console.log('here are some cat facts')
@@ -13,7 +13,6 @@ let fetchData = () => {
     fetch(BASE_URL)
     .then(response => response.json())
     .then(data => renderFacts(data))
-
 }
 
 let renderFacts = (data) => {
@@ -24,11 +23,13 @@ let renderFacts = (data) => {
         const newLi = document.createElement('li')
         newLi.textContent = fact.text
         factsUl.appendChild(newLi)
-        
     })
-    
+}
 
+let closeWindow = () => {
+    popUp.remove()
 }
 
 getFacts.addEventListener('click', fetchData)
+closeButton.addEventListener('click', closeWindow)
 document.addEventListener('DOMContentLoaded', init);
